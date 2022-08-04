b0cda6407c5210d912aa74117a6e507c9c16dfcd
Diar Nur Rizky <drawxd4w@gmail.com>
2022-08-04 22:14:54 +0700

    about app

--- lib/app/modules/about_app/controllers/about_app_controller.dart
+++ lib/app/modules/about_app/controllers/about_app_controller.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
 
 class AboutAppController extends GetxController {
+  final RxString aboutApp =
+      "Aplikasi ini merupakan aplikasi berbasis Android, dimana aplikasi ini berfungsi untuk mengamankan sebuah file dokumen agar file dokumen tidak dapat terbaca oleh pihak yang tidak berkenan.\nAdapun proses dalam aplikasi ini terdapat dua proses yaitu proses enkripsi dan dekripsi\n\nAplikasi ini dikembangkan oleh\nRita Bonita (24052218086)\nProgram Studi Teknik Elektro\nFakultas Teknik\nUniversitas Garut\nEmail ritabonita1312@gmail.com"
+          .obs;
   @override
   void onInit() {
     super.onInit();
--- lib/app/modules/about_app/views/about_app_view.dart
+++ lib/app/modules/about_app/views/about_app_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 
@@ -7,15 +8,85 @@ import '../controllers/about_app_controller.dart';
 class AboutAppView extends GetView<AboutAppController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('AboutAppView'),
-        centerTitle: true,
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    double top = MediaQuery.of(context).padding.top;
+    Size size = MediaQuery.of(context).size;
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.dark,
+        statusBarColor: Colors.transparent,
       ),
-      body: Center(
-        child: Text(
-          'AboutAppView is working',
-          style: TextStyle(fontSize: 20),
+      child: Scaffold(
+        body: Container(
+          height: size.height,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.teal[50]!,
+                Colors.teal[200]!,
+              ],
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: top,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+                child: GestureDetector(
+                  onTap: () {
+                    Get.back();
+                  },
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.chevron_left_rounded,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'Kembali',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                child: Text(
+                  "Tentang Aplikasi",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(16, 0, 16, 0),
+                child: Text(
+                  controller.aboutApp.value,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
